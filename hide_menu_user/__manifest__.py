e75605c062c90452283c59e31e5da685ee3f6567
anandodoo <mcanand1998@gmail.com>
2023-01-13 01:10:09 +0530

    [ADD][hide menu]dev

---
+++ hide_menu_user/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Hide Any Menu User Wise',
+    'version': '15.0.1.0.0',
+    'summary': 'Hide Any Menu Item User Wise',
+    'description': 'Hide Any Menu Item User Wise, Hide Menu Items, Hide Menu',
+    'category': 'Extra Tools',
+    'author': 'ANAND MC',
+    'depends': ['base'],
+    'data': [
+        'views/res_users.xml',
+        'security/security.xml'
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

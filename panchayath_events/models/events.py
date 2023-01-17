76c15470151240367d603d0ba6be0e64ee88620d
amrita99 <amrithams99@gmail.com>
2023-01-17 22:47:40 +0530

    [FIX][Panchayath events]

--- panchayath_events/models/events.py
+++ panchayath_events/models/events.py
@@ -13,27 +13,41 @@ class PanchayathEvents(models.Model):
     panchayath_assis = fields.Many2one('res.partner', string='Panchayath assistance', required=True)
     image = fields.Binary(string='image')
     schedule_date = fields.Datetime(string="Schedule Date", required=True)
-    category = fields.Selection([('emergency', 'Emergency'), ('health', 'Health')],required=True)
-    priority = fields.Integer(string='priority' ,required=True)
-    heading =fields.Text(string="Heading", required=True)
-    subheading =fields.Text(string="Sub heading", required=True)
-    message =fields.Html(string="Message", required=True)
+    category = fields.Selection([('emergency', 'Emergency'), ('health', 'Health')], required=True)
+    priority = fields.Integer(string='priority', required=True)
+    heading = fields.Text(string="Heading", required=True)
+    subheading = fields.Text(string="Sub heading", required=True)
+    message = fields.Html(string="Message", required=True)
     state = fields.Selection([
         ('not_published', ' Not Published'),
         ('published', ' Published'),
 
-    ], string='Status', default="not_published",readonly=True)
-
+    ], string='Status', default="not_published", readonly=True)
 
     @api.onchange('category')
     def onchange_age(self):
-        self.priority = 2 if not self.category =='emergency' else 1
+        self.priority = 2 if not self.category == 'emergency' else 1
 
     def write(self, vals):
-        res = super(PanchayathEvents,self).write(vals)
-        if self.priority >1:
-            raise ValidationError(('You should select at least one Advanced Presence Control option.'))
+        if self.priority > 1 and self.category == 'emergency':
+            raise ValidationError(('Priority of emergency must be 1'))
+        if self.priority == 0 and self.category == 'emergency':
+            raise ValidationError(('Priority of emergency will not be equal to 0'))
+        elif self.priority < 2 and self.category != 'emergency':
+            raise ValidationError(('Priority of health will not be eqal to 1'))
+        res = super(PanchayathEvents, self).write(vals)
+        return res
 
+    @api.model_create_multi
+    def create(self, vals):
+        res = super(PanchayathEvents, self).create(vals)
+        if self.priority > 1 and self.category == 'emergency':
+            raise ValidationError(('Priority of emergency must be 1'))
+        if self.priority == 0 and self.category == 'emergency':
+            raise ValidationError(('Priority of emergency will not be equal to 0'))
+        elif self.priority < 2 and self.category != 'emergency':
+            raise ValidationError(('Priority of health will not be eqal to 1'))
+        # res = super(PanchayathEvents, self).create(vals)
         return res
 
     def post(self):
@@ -45,12 +59,3 @@ class PanchayathEvents(models.Model):
         print("sa")
         if self.state == 'published':
             self.state = 'not_published'
-
-
-
-
-
-
-
-
-

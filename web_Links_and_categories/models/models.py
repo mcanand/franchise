8d79fa632efca5d92f594befcc2ff762d4284629
amrita99 <amrithams99@gmail.com>
2023-01-31 21:24:52 +0530

    [FIX][web links and categories]

--- web_Links_and_categories/models/models.py
+++ web_Links_and_categories/models/models.py
@@ -13,26 +13,36 @@ class ProductTemplateInherit(models.Model):
 
 class FranchiseApplicationPartners(models.Model):
     _name = 'franchise.application.partner'
-    _description = 'user application for franchise'
+    _description = 'user application for registration'
 
-    application_sequence = fields.Char(string="Application Number", readonly=True, required=True, copy=False,
-                                       default='New')
+    application_sequence = fields.Char(string="Application Number", readonly=True, required=True, copy=False, default='New')
     name = fields.Char(string="Applicant Name")
     district_id = fields.Many2one('res.country.state.district', string="District")
+    dob = fields.Date(string="Date of Birth", required=True)
+    related_users_id = fields.Many2one('res.users', string="Related Users")
+    age = fields.Integer(string="AGE", compute="_check_age", store=True)
     mobile = fields.Char(string="Mobile")
     email = fields.Char(string="Email")
     panchayat_id = fields.Many2one('res.country.state.district.panchayat', string="Panchayat")
     location = fields.Char(string="Location prefered")
     known_by = fields.Char(string="Known through")
     status = fields.Selection(
-        [('draft', "Draft"), ('progress', 'In Progress'), ('done', 'Approved')], default='draft')
+        [('draft', "Draft"),('progress','In Progress'), ('done', 'Approved')], default='draft')
     color = fields.Integer('Color', compute='_get_color', store=True)
+    local_body = fields.Selection([("panchayath", "Panchayath"), (" municipality", "Municipality"), ("corporation", "Corporation")])
+
+    @api.depends('dob')
+    def _check_age(self):
+        today = fields.date.today()
+        age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
+        print('age', age)
+
 
     @api.model
     def create(self, vals):
-        if vals.get('application_sequence', 'New') == 'New':
-            vals['application_sequence'] = self.env['ir.sequence'].next_by_code(
-                'franchise.application.partner') or 'New'
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code(
+                'registration.application.partner') or 'New'
         result = super(FranchiseApplicationPartners, self).create(vals)
         return result
 
@@ -47,6 +57,33 @@ class FranchiseApplicationPartners(models.Model):
                 rec.color = 10
 
 
+    def check(self):
+        print("h")
+        if self.status == 'draft':
+            self.status = 'progress'
 
 
+    def approve(self):
+        print("hhjj")
+        res_users = self.env['res.users'].create({
+            'name': self.name,
+            'login': self.mobile,
+            'sel_groups_1_9_10': '1',
+            'panchayat_admin': True,
+            'district_id':self.district_id.id,
+            'panchayat_id': self.panchayat_id.id,
+
+        })
+        res_users.id = self.write({'related_users_id':res_users})
+
+        # return super(FranchiseApplicationPartners, self).create(res_users)
+        if self.status == 'progress':
+            self.status = 'done'
+        # return super(FranchiseApplicationPartners, self).create(res_users)
+
+
+    def cancel(self):
+        print("dfsda")
+        if self.status == 'done':
+            self.status = 'draft'
 
